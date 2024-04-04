212b3a2ec7e5b293775bbee8e71816ab8f1f7e6c
AngelHCS <8990holbertonstudents.com>
2024-04-03 20:44:22 -0400

    Fixed output

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,16 +8,15 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
+	if (tree)
+	{
+		size_t lef = 0, rig = 0;
 
-	if (tree == NULL)
-		return (0);
+		lef = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		rig = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+		return ((lef > rig) ? lef : rig);
+	}
 
-	if (left_height > right_height)
-		return (left_height + 1);
-	else
-		return (right_height + 1);
+	return (0);
 }
