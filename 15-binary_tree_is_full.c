2dbce893c5e1c0701d472284f7f8f2cb7cdf7edb
AngelHCS <8990holbertonstudents.com>
2024-04-04 17:51:28 -0400

    15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -5,7 +5,7 @@
 *@tree: Pointer to the root node of the tree to check
 *Return: 1 if the tree is full, 0 otherwise
 */
-int binary_tree_is_full(const binary_tree_t tree)
+int binary_tree_is_full(const binary_tree_t *tree)
 {
     if (tree == NULL)
         return (0);
