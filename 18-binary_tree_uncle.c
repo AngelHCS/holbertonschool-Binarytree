f14a140475a6fd7ff777b93a6b124ecc3f822818
AngelHCS <159042316+AngelHCS@users.noreply.github.com>
2024-04-04 19:44:41 -0400

    Update 18-binary_tree_uncle.c (second try)

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -8,21 +8,21 @@
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
-		return (NULL);
-	binary_tree_t *parent = node->parent;
-	binary_tree_t *grandparent = parent->parent;
+        if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+                return (NULL);
+        binary_tree_t *parent = node->parent;
+        binary_tree_t *grandparent = parent->parent;
 
-	if (grandparent->left == parent)
-	{
-		return (grandparent->right);
-	}
-		else if (grandparent->right == parent)
-	{
-		return (grandparent->left);
-	}
-		else
-	{
+        if (grandparent->left == parent)
+        {
+                return (grandparent->right);
+        }
+                else if (grandparent->right == parent)
+        {
+                return (grandparent->left);
+        }
+                else
+        {
 return (NULL);
-	}
+        }
 }
