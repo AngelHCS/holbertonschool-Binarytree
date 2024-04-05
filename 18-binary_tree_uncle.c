b099af0be8ece95b33bee15a8d41b2507aa0e504
AngelHCS <8990holbertonstudents.com>
2024-04-04 20:24:11 -0400

    finally should work with ISO C90

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -8,22 +8,25 @@
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
+	binary_tree_t *parent, *grandparent;
+
 	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+
 		return (NULL);
-	binary_tree_t *parent = node->parent;
-	binary_tree_t *grandparent = parent->parent;
+
+	parent = node->parent;
+	grandparent = parent->parent;
 
 	if (grandparent->left == parent)
-	{
+    {
 		return (grandparent->right);
-	}
-		else if (grandparent->right == parent)
-	{
+    }
+	else if (grandparent->right == parent)
+    {
 		return (grandparent->left);
-	}
-		else
-	{
-return (NULL);
+    }
+	else
+    {
+		return (NULL);
 	}
 }
-
